7d08ff8900a45c341a51c00915326285e1e809e5
Christoffer Riis <Christoffer@Riis.cc>
2023-06-10 10:57:47 +0200

    Test chore db

--- Backend/Models/ChoreModel.cs
+++ Backend/Models/ChoreModel.cs
@@ -1,6 +1,6 @@
-
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
 namespace Backend.Models;
 
 public class ChoreModel
@@ -12,5 +12,4 @@ public class ChoreModel
     public int FrequencyInDays { get; set; }
     public UserModel? AssignedTo { get; set; }
     public DateTime? LastCompleted { get; set; }
-
 }
